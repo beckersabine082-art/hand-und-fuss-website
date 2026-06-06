4794da663ac5df514740a2a8d52ef1bf7b6870f9
beckersabine082-art <beckersabine082@gmail.com>
2026-06-06 15:09:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ if (startBanner) {
         startBanner.classList.add("hide");
     }, { once: true });
 }
+const startBanner = document.getElementById("start-banner");
+
+if (startBanner) {
+    startBanner.addEventListener("click", () => {
+        startBanner.classList.add("hide");
+    });
+
+    document.addEventListener("click", (event) => {
+        if (!startBanner.contains(event.target)) {
+            startBanner.classList.add("hide");
+        }
+    });
+
+    window.addEventListener("scroll", () => {
+        startBanner.classList.add("hide");
+    }, { once: true });
+}
