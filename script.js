886ba6cd058cba00e11e5b990813c81498f6d35c
beckersabine082-art <beckersabine082@gmail.com>
2026-06-06 15:49:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const startPopup = document.getElementById("start-popup");
-const closePopup = document.getElementById("close-popup");
 
-if (startPopup && closePopup) {
-    closePopup.addEventListener("click", () => {
-        startPopup.classList.add("hide");
-    });
-
-    startPopup.addEventListener("click", (event) => {
-        if (event.target === startPopup) {
-            startPopup.classList.add("hide");
-        }
-    });
-
-    window.addEventListener("scroll", () => {
-        startPopup.classList.add("hide");
-    }, { once: true });
-}
